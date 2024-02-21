0bca06dea7819c9e104d18978c029c69b52c5ae9
Boikhutso Mokoka <boikhuso.m@gmail.com>
2024-02-21 16:32:29 +0200

    js fix error 2

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ const navLinks = document.querySelectorAll('.nav__link')
 
 navToggle.addEventListener('click', () => {
     document.body.classList.toggle('nav-open');
-});
+})
 
 navLinks.forEach(link => {
     link.addEventListener('click', () => {
